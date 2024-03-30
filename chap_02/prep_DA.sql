74ae7f6724d55ad13ab84686293b6454a164f663
Joaquin Romero Flores <jromerof30@gmail.com>
2024-03-30 11:54:48 -0600

    Prearing: Shaping data

--- chap_02/prep_DA.sql
+++ chap_02/prep_DA.sql
@@ -39,9 +39,128 @@ GROUP BY 1;
 ####
 
 # n-Tiles
+# window function syntax as follow:
+
+-- function[SUM, COUNT, AVG, MIN, MAX](field_name) OVER (PARITITON BY field_name
+-- 							ORDER BY field_name);
+
+#n-tile procedure:
+-- NTILE(num_bins) OVER (PARTITION BY...ORDER BY...)
+
+-- SELECT NTILE,
+-- 		MIN() AS lower_bound,
+--         MAX() AS upper_bound,
+--         COUNT(order_id) AS orders
+-- 	FROM
+-- 		(SELECT customer_id, order_id, arder_amount,
+--         NTILE(10) OVER (ORDER BY order_amount) AS ntile
+--         FROM orders
+--         )
+-- GROUP BY 1
+-- ;
+
+# percent_rank procedure:
+-- PERCENT_RANK() OVER (PARTITION BY... ORDER BY...)
+
+# Detecting Duplicates
+-- checking for duplicates:
+SELECT customer_id, first_name, last_name
+	FROM customer
+GROUP BY 1, 2, 3;
+
+
+SELECT COUNT(*)
+	FROM 
+		(SELECT customer_id, first_name, last_name
+			FROM customer
+		GROUP BY 1, 2, 3
+        ) AS cust_all
+WHERE store_id > 1;
+
+
+-- SELECT records, COUNT(*)
+-- 	FROM
+-- 		(SELECT column_a, column_a, column_a..., COUNT(*) AS records
+-- 			FROM
+-- 		GROUP BY 1, 2, 3...
+--         ) AS a
+-- WHERE records > 1
+-- 	GROUP BY 1
+
+
+-- SELECT column_a, column_a, column_a..., COUNT(*) AS records
+-- 	FROM
+-- GROUP BY 1, 2, 3...
+-- HAVING COUNT(*) >1;
+
+# Deduplicatio with GROUP BY & DISTINCT
+
+SELECT c.customer_id, c.first_name, c.last_name
+	FROM customer AS c
+    INNER JOIN payment AS p
+		ON P.customer_id = p.customer_id;
+
+SELECT DISTINCT c.customer_id, c.first_name, c.last_name
+	FROM customer AS c
+    INNER JOIN payment AS p
+		ON P.customer_id = p.customer_id;
+
+
+SELECT customer_id,
+		MIN(amount) AS first_transaction_date,
+        MAX(amount) AS last_transaction_date,
+        COUNT(amount) AS total_amount
+	FROM payment
+GROUP BY customer_id;
+
+
+# Preparing: Data Cleaning
+
+# Cleanining data with CASE transformations
+
+-- SELECT rating,
+-- 		CASE WHEN rating  = 'PG-13' THEN 'Available'
+--         CASE WHEN rating  = 'NC-17' THEN 'Scarse'
+--         CASE WHEN rating  = 'R' THEN 'Non-Available'
+-- 		ELSE rating
+-- 	END AS rating_options
+-- 	FROM film;
+	
+# Type Conversion & Casting
+
+-- cast(1234 AS VARCHAR)
+
+-- CASE order_items WEHN <= 3 THEN order_items::VARCHAR
+-- 	ELSE
+-- END
+
+-- SELECT REPLACE('$19.99', '$', '');
+
+
+# Dealing with Nulls: COALESCE, NULLIF, nvl Functions
+
+-- CASE WHEN num_order IS NULL 
+-- 	THEN 0 
+-- 		ELSE num_order 
+-- END
+
+-- COALESCE(num_order, 0)
+
+-- NULLIF(6, 7)
+
+-- NULLIF(date, '1970-01-01')
+
+# Missing Data
 
 
 
+
+
+
+
+
+
+###########
 
 
 -- DROP table if exists public.date_dim;
